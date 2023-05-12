20e1b5a1a4193580430b6c67880e1e47ba79d8ff
harith01 <harithyusuph@gmail.com>
2023-05-12 17:29:59 +0000

    Create console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Creates a Console"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the console class"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+    def emptyline(self):
+        """Empty line command to not execute anything"""
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
